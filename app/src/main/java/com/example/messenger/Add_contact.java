e2c6308228a59e48952494fd7b411573b4b71ed1
grnd-alt <salim.belakkaf@gmx.de>
2020-06-22 10:47:01 +0200

    "adding androit studio files"

--- app/src/main/java/com/example/messenger/Add_contact.java
+++ app/src/main/java/com/example/messenger/Add_contact.java
@@ -3,14 +3,18 @@ package com.example.messenger;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
@@ -27,6 +31,33 @@ public class Add_contact extends AppCompatActivity {
                 search.execute();
             }
         });
+        final Button send_request = (Button) findViewById(R.id.send);
+        send_request.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (send_request.getVisibility() == View.VISIBLE){
+                    TextView userdis = (TextView) findViewById(R.id.user_name_display);
+                    AsyncTask request = new request();
+                    request.execute(userdis.getText().toString());
+                }
+                else{
+                    return;
+                }
+            }
+        });
+    }
+    private class request extends AsyncTask{
+        ProgressDialog pd;
+        @Override
+        protected void onPreExecute(){
+            pd = pd.show(Add_contact.this,"","checking registration data",true,true);
+            return;
+        }
+        @Override
+        protected Object doInBackground(Object ... objects){
+            add_contact((String) objects[0]);
+            return null;
+        }
     }
     private class searcher extends AsyncTask{
         ProgressDialog pd;
@@ -61,7 +92,20 @@ public class Add_contact extends AppCompatActivity {
         protected void onPostExecute(Object result){
             TextView user_display = (TextView) findViewById(R.id.user_name_display);
             user_display.setText(result.toString());
+            Button search_input = (Button) findViewById(R.id.send);
+            search_input.setVisibility(View.VISIBLE);
             pd.dismiss();
+        }
+    }
+    public void add_contact(String Username){
+        try{
+            FileOutputStream fos = openFileOutput("contacts.txt", Context.MODE_APPEND);
+            String save = Username +';';
+            fos.write(save.getBytes());
+            fos.close();
+            TextView username = (TextView) findViewById(R.id.username_input);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
